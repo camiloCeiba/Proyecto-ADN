6d034746be41a2e2fc112393903ae9a4fd0b3afb
Camilo Sanchez <camilo.sanchez@ceiba.com.co>
2021-10-06 11:17:17 -0500

    Se realiza ajuste de test crear

--- src/app/feature/admin-libro/components/crear-producto/crear-producto.component.spec.ts
+++ src/app/feature/admin-libro/components/crear-producto/crear-producto.component.spec.ts
@@ -10,6 +10,7 @@ describe('CrearProductoComponent', () => {
   let component: CrearProductoComponent;
   let fixture: ComponentFixture<CrearProductoComponent>;
   let libroService: LibroService;
+  // let redirectPage;
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CrearProductoComponent ],
@@ -29,6 +30,7 @@ describe('CrearProductoComponent', () => {
     fixture = TestBed.createComponent(CrearProductoComponent);
     component = fixture.componentInstance;
     libroService = TestBed.inject(LibroService);
+    // redirectPage = spyOn(component, 'redirigir');
     spyOn(libroService, 'crear').and.callThrough();
     fixture.detectChanges();
   });
@@ -39,5 +41,24 @@ describe('CrearProductoComponent', () => {
 
   it('formulario es invalido cuando esta vacio', () => {
     expect(component.libroForm.valid).toBeFalsy();
+  });
+  it('Registrando producto', () => {
+    expect(component.libroForm.valid).toBeFalsy();
+    component.libroForm.controls.id.setValue(99);
+    component.libroForm.controls.nombreLibro.setValue('Libro test');
+    component.libroForm.controls.editorial.setValue('editorial Test');
+    component.libroForm.controls.URL.setValue('URL test');
+    component.libroForm.controls.resumen.setValue('resumen test');
+    component.libroForm.controls.categoria.setValue('categoria test');
+    component.libroForm.controls.fechaPublicacion.setValue(1905);
+    component.libroForm.controls.estado.setValue('Disponible');
+    component.libroForm.controls.valorDia.setValue(700);
+    component.libroForm.controls.codigoLibro.setValue(24587);
+    expect(component.libroForm.valid).toBeTruthy();
+
+    component.crear(component.libroForm.valid);
+    // expect(redirectPage).toHaveBeenCalled();
+    // Aca validamos el resultado esperado al enviar la petición
+    // TODO adicionar expect
   });
 });
